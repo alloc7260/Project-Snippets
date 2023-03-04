0436a6c6110bb330c232e5d549ddac8215b307f7
Pranav Patel <101977910+alloc7260@users.noreply.github.com>
2023-03-04 19:11:50 +0530

    added code snippet to execute code without any error

---
+++ execute code without any error/execute_code_without_any_error.py
@@ -0,0 +1,12 @@
+while True:
+    try:
+        # Put your code here that you want to run repeatedly
+        # until it runs without errors
+        # ...
+        
+        # If your code executes successfully, break out of the loop
+        break
+    
+    except:
+        # If an error occurs, continue the loop and try again
+        continue
